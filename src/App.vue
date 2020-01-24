c5564d7fe1931ddec4258fa2e20f074e767972a0
bemijonathan <bemijonathan@gmail.com>
2020-01-24 16:01:44 +0100

    set up tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
+// eslint-disable-next-line vue/valid-template-root
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<div>
+<h1 class="bg-green-300"> Hello </h1>
+</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
